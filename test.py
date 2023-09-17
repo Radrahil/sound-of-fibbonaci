fc1610c21b02b7ae3b041c1cc9f1db92226a266a
Rahil Prakash <54015030+Radrahil@users.noreply.github.com>
2023-09-17 13:44:14 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,41 @@
+import musicalbeeps
+
+
+player = musicalbeeps.Player(volume=1, mute_output=False)
+
+player.play_note("C", 0.2)
+
+fib1 = 0
+fib2 = 1
+
+for i in range(100):
+    tempfib = fib1 + fib2
+    fib1 = fib2 % 12
+    fib2 = tempfib % 12
+    match fib1:
+        case 0:
+            player.play_note("C", 0.2)
+        case 1:
+            player.play_note("C#", 0.2)
+        case 2:
+            player.play_note("D", 0.2)
+        case 3:
+            player.play_note("D#", 0.2)
+        case 4:
+            player.play_note("E", 0.2)
+        case 5:
+            player.play_note("F", 0.2)
+        case 6:
+            player.play_note("F#", 0.2)
+        case 7:
+            player.play_note("G", 0.2)
+        case 8:
+            player.play_note("G#", 0.2)
+        case 9:
+            player.play_note("A", 0.2)
+        case 10:
+            player.play_note("A#", 0.2)
+        case 11:
+            player.play_note("B", 0.2)
+        case other:
+            pass
